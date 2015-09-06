109b0ce6eab16fb45af9b81b48877695dd449b9c
npsuzan <np.suzan@gmail.com>
2015-09-06 19:30:50 +0545

    Initial commit

---
+++ app/src/main/java/com/example/nazus/practice/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.nazus.practice;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class
+        MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+   }
+
